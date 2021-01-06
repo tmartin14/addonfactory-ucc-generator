723be33593308111a8411ef3365e8c9673532a74
mamin-crest <mamin@splunk.com>
2021-01-06 13:41:44 +0530

    Removed configuraion for unglyfyig

--- splunk_add_on_ucc_framework/UCC-UI-lib/config/webpack/buildConfig.js
+++ splunk_add_on_ucc_framework/UCC-UI-lib/config/webpack/buildConfig.js
@@ -6,9 +6,6 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin('common.js'),
         new webpack.DefinePlugin({
             __CONFIG_FROM_FILE__: false
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {warnings: false}
         })
     ]
 };
